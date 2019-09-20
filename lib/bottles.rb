cb53b8c961e8b8aa7c1f493fd826fb9893bf6d99
Zach Rose <zach.rose@rigup.com>
2019-09-20 14:30:53 -0500

    minor refactor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,12 +5,7 @@ class Bottles
   end
 
   def verse(n)
-    if(n == 99)
-      <<~LYRIC
-        99 bottles of beer on the wall, 99 bottles of beer.
-        Take one down and pass it around, 98 bottles of beer on the wall.
-      LYRIC
-    elsif(n == 2)
+    if(n == 2)
       <<~LYRIC
         2 bottles of beer on the wall, 2 bottles of beer.
         Take one down and pass it around, 1 bottle of beer on the wall.
