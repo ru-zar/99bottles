0fd45e46a9dae0ad9bbe20c2c96c3bbfd0c25c88
Zach Rose <zach.rose@rigup.com>
2019-09-20 14:08:56 -0500

    first example

---
+++ lib/bottles.rb
@@ -0,0 +1,8 @@
+class Bottles
+  def verse(a)
+    <<~LYRIC
+      99 bottles of beer on the wall, 99 bottles of beer.
+      Take one down and pass it around, 98 bottles of beer on the wall.
+    LYRIC
+  end
+end
